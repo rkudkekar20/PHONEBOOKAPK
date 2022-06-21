7d48800000f6219c1f22f62f23206f9d9a5264c0
rameshwar kudkekar <you@example.com>
2022-06-21 23:52:53 +0530

    PBA-5 make service interface and declare mathod as requirements

--- src/main/java/com/bikkadIt/service/ContactServiceI.java
+++ src/main/java/com/bikkadIt/service/ContactServiceI.java
@@ -1,5 +1,12 @@
 package com.bikkadIt.service;
 
+import java.util.List;
+
+import com.bikkadIt.model.Contact;
+
 public interface ContactServiceI {
+	
+	boolean saveContact(Contact contact);
+	List<Contact>getAllContact();
 
 }
--- src/main/java/com/bikkadIt/service/ContactServiceImpl.java
+++ src/main/java/com/bikkadIt/service/ContactServiceImpl.java
@@ -1,5 +1,37 @@
 package com.bikkadIt.service;
 
-public class ContactServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bikkadIt.model.Contact;
+import com.bikkadIt.repository.CurdRepository;
+
+@Service
+public class ContactServiceImpl implements ContactServiceI{
+	
+	@Autowired
+	private CurdRepository curdrepository;
+	
+	@Override
+	public boolean saveContact(Contact contact) {
+		Contact save = curdrepository.save(contact);
+		if(save!=null) {
+			return true;
+		}else
+
+		return false;
+	}
+
+	@Override
+	public List<Contact> getAllContact() {
+		List<Contact> contact = curdrepository.findAll();
+		if(contact!=null) {
+			return contact;
+		}else
+
+		return null;
+	}
 
 }
