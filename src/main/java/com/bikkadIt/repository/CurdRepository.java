4078c4aedff02e0325a8908990cd6837a22c1bb2
rameshwar kudkekar <you@example.com>
2022-06-21 23:32:40 +0530

    PBA-6 create jpa repository

--- src/main/java/com/bikkadIt/repository/CurdRepository.java
+++ src/main/java/com/bikkadIt/repository/CurdRepository.java
@@ -1,9 +1,14 @@
 package com.bikkadIt.repository;
 
+import java.io.Serializable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bikkadIt.model.Contact;
 
-public interface CurdRepository extends JpaRepository<Contact, Integer>{
+@Repository
+public interface CurdRepository extends JpaRepository<Contact, Serializable>{
+	
 
 }
